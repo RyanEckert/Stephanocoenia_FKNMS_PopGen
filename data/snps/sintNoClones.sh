7a47d5770cca6ec84f0735b53422b444266459fa
RyanEckert <ryan.j.eckert@gmail.com>
2023-09-01 13:34:32 -0400

    update for manuscript submission

--- data/snps/sintNoClones.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-srun angsd -b bamsNoClones -GL 1 -uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 30 -dosnpstat 1 -doHWE 1 -hwe_pval 1e-5 -sb_pval 1e-5 -hetbias_pval 1e-5 -skipTriallelic 1 -minInd 187 -snp_pval 1e-6 -minMaf 0.05 -doMajorMinor 1 -doMaf 1 -doCounts 1 -makeMatrix 1 -doIBS 1 -doCov 1 -doGeno 8 -doBcf 1 -doPost 1 -doGlf 2 -P 1 -out sintNoClones
